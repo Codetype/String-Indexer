50ea1099ac44b8c8ee13edd11b7d70b61ecec84a
Codetype <pawelgedlek@gmail.com>
2019-08-03 12:46:34 +0200

    test cases resolved

--- src/main/java/pl/pgedlek/StringIndexer.java
+++ src/main/java/pl/pgedlek/StringIndexer.java
@@ -1,6 +1,9 @@
 package pl.pgedlek;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 class StringIndexer {
     private String input;
@@ -13,8 +16,52 @@ class StringIndexer {
         this.input = input;
     }
 
+    private String buildResult(Map<Character, ArrayList<String>> letterOccurenceMap) {
+        final String squareBracketsRegex = "[\\[\\]]";
+        StringBuilder stringResult = new StringBuilder();
+        for (Map.Entry<Character, ArrayList<String>> entry : letterOccurenceMap.entrySet()) {
+            stringResult.append(entry.getKey());
+            stringResult.append(": ");
+            stringResult.append(entry.getValue().toString().replaceAll(squareBracketsRegex, ""));
+            stringResult.append("\n");
+        }
+
+        return stringResult.toString();
+    }
+
     String transform() {
-        String result = "";
-        return result;
+        final String specialCharactersRegex = "[^a-zA-Z0-9\\s+]";
+        final String inputCharacters = "abcdefghijklmnopqrstuvwxyz";
+        Map<Character, ArrayList<String>> letterOccurenceMap = new LinkedHashMap<Character, ArrayList<String>>();
+
+        if(input.equals("")) {
+            return input;
+        }
+
+        input = input.toLowerCase();
+        input = input.replaceAll(specialCharactersRegex, "");
+
+        String[] words = input.split(" ");
+
+        for(Character letter : inputCharacters.toCharArray()){
+            for(String word : words) {
+                if(word.contains(letter.toString())){
+                    ArrayList<String> letterWordsList = letterOccurenceMap.get(letter);
+
+                    if(letterWordsList == null){
+                        letterWordsList = new ArrayList<String>();
+                    }
+
+                    if(!letterWordsList.contains(word)){
+                        letterWordsList.add(word);
+                        Collections.sort(letterWordsList);
+                    }
+
+                    letterOccurenceMap.put(letter, letterWordsList);
+                }
+            }
+        }
+
+        return buildResult(letterOccurenceMap);
     }
 }
