030a70232d93e04c9518e6562fe4cc5c6b78017f
Codetype <pawelgedlek@gmail.com>
2019-08-03 12:44:58 +0200

    test cases created

---
+++ src/main/java/pl/pgedlek/StringIndexer.java
@@ -0,0 +1,20 @@
+package pl.pgedlek;
+
+import java.util.*;
+
+class StringIndexer {
+    private String input;
+
+    StringIndexer(String input){
+        this.input = input;
+    }
+
+    void setInput(String input) {
+        this.input = input;
+    }
+
+    String transform() {
+        String result = "";
+        return result;
+    }
+}
