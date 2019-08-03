fb21f5477cfadd26d3bc6fddd92ba139ed867b69
Codetype <pawelgedlek@gmail.com>
2019-08-03 12:47:59 +0200

    example input added

--- src/main/java/pl/pgedlek/Main.java
+++ src/main/java/pl/pgedlek/Main.java
@@ -2,6 +2,9 @@ package pl.pgedlek;
 
 public class Main {
     public static void main(String[] args) {
+        String input = "ala ma kota, kot koduje w Javie kota";
+        StringIndexer stringIndexer = new StringIndexer(input);
 
+        System.out.println(stringIndexer.transform());
     }
 }
